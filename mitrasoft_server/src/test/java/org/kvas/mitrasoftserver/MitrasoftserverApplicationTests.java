6526ed827ec675eba695b6b4e0564c88ec847130
Kvas <shokhin.al@gmail.com>
2022-03-26 13:34:50 +0300

    REST server tests

--- mitrasoft_server/src/test/java/org/kvas/mitrasoftserver/MitrasoftserverApplicationTests.java
+++ mitrasoft_server/src/test/java/org/kvas/mitrasoftserver/MitrasoftserverApplicationTests.java
@@ -7,7 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import javax.transaction.Transactional;
 
@@ -17,6 +19,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+@Sql("/data.sql")
 class MitrasoftserverApplicationTests {
 
     @Autowired
@@ -24,13 +27,37 @@ class MitrasoftserverApplicationTests {
 
 
     @Test
-    void testDummy() throws Exception {
+    void testGetMessages() throws Exception {
         MockHttpServletResponse response = mockMvc
                 .perform(get("/rest/message"))
                 .andReturn()
                 .getResponse();
 
         assertEquals(200, response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON.toString(), response.getContentType());
+        assertThat(response.getContentAsString().contains("Lorem"));
+        assertThat(response.getContentAsString().contains("Ipsum"));
     }
 
+    @Test
+    void testAddMessage() throws Exception {
+        MockHttpServletResponse responsePost = mockMvc
+                .perform(post("/rest/message")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"message\":\"hello, world\"}")
+                )
+                .andReturn()
+                .getResponse();
+
+        assertEquals(200, responsePost.getStatus());
+
+        MockHttpServletResponse response = mockMvc
+                .perform(get("/rest/message"))
+                .andReturn()
+                .getResponse();
+
+        assertEquals(200, response.getStatus());
+        assertEquals(MediaType.APPLICATION_JSON.toString(), response.getContentType());
+        assertThat(response.getContentAsString().contains("hello, world"));
+    }
 }
---
+++ mitrasoft_server/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO messages (ID, MESSAGE, CREATED_AT) VALUES
+    (1, 'Lorem', '2022-03-26 13:20:00'),
+    (2, 'Ipsum', '2022-03-26 13:20:00');
